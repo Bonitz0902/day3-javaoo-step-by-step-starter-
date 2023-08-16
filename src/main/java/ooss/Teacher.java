f761ef427f0ae971e702529aac67dfbd1067a9d7
Guile <jimenezguiles7255@gmail.com>
2023-08-16 17:23:43 +0800

    FEAT: passed the should_return_message_with_name_and_age_when_introduce_given_person

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,9 +1,7 @@
 package ooss;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Teacher extends Person {
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
 }
