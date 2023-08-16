df8afc92ef457818b22c8e301abba24c657e3808
Guile <jimenezguiles7255@gmail.com>
2023-08-16 22:28:03 +0800

    FEAT: passed the Step 3

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,12 +1,23 @@
 package ooss;
 
 public class Student extends Person {
+    private Klass klass;
 
     public Student(int id, String name, int age) {
+
         super(id, name, age);
     }
     @Override
     public String introduce(){
-        return super.introduce()+ " I am a student.";
+        return super.introduce()+
+                String.format(" I am a student. I am in class %d.",klass.getId());
+    }
+
+    public void join(Klass klass){
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return this.klass != null && this.klass.getId() == klass.getId();
     }
 }
